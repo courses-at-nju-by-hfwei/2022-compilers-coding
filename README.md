88d8ca2b4be0c552c3d6bf7a7398c9bab79bc495
Hengfeng Wei <hengxin>
2022-11-08 12:52:13 +0800

    +1-lexer-antlr

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 # compilers-antlr
 
-Using [ANTLR4](https://www.antlr.org/) in the Course on Compiler Theory at software.nju.edu.cn
+> Using [ANTLR4](https://www.antlr.org/)
+> in the [Compilers Course](http://47.122.3.40:8081/#/) @ software.nju.edu.cn
+
+## Structures
 
 - src
-  - main 
-    - antlr
-      - simpleexpr (gramma SimpleExpr)
-      - simpleexprlexer (lexer grammar SimpleExprLexer)
-    - java
-      - dragon (The Tiny Compiler in The Dragon Book)
-        - lexer (TODO: It is incomplete. You are welcome to finish it.)
-        - parser (TODO)
-      - simpleexpr
-      - simpleexprlexer (lexer grammar testing)
-- test
-  - antlr
-    - antlr
-      - dragon
-  - java
-    - dragon (TestNG for dragon)
+    - main
+        - antlr
+            - hello (`Hello.g4`)
+            - intexpr (`IntExpr.g4`)
+            - simpleexpr (`SimpleExpr.g4`)
+            - simpleexprlexer (`SimpleExprLexer.g4`)
+        - java
+            - simpleexpr (`SimpleExprTest.java`)
+            - simpleexprlexer (`SimpleExprLexer.java`)
+    - test
+        - antlr
+        - java
--- src/main/java/simpleexprlexer/SimpleExprLexerTest.java
+++ src/main/java/simpleexprlexer/SimpleExprLexerTest.java
@@ -2,31 +2,29 @@ package simpleexprlexer;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.tree.ParseTree;
-import simpleexpr.SimpleExprLexer;
-import simpleexpr.SimpleExprParser;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import simpleexpr.SimpleExprLexer;
+
 public class SimpleExprLexerTest {
-    public static void main(String[] args) throws IOException {
-        InputStream is = System.in;
+  public static void main(String[] args) throws IOException {
+    InputStream is = System.in;
 
-        String file;
-        if (args.length > 0) {
-            file = args[0];
-            is = new FileInputStream(file);
-        }
-
-        CharStream input = CharStreams.fromStream(is);
-        SimpleExprLexer lexer = new SimpleExprLexer(input);
-
-        for (Token token : lexer.getAllTokens()) {
-            System.out.println(token);
-        }
+    String file;
+    if (args.length > 0) {
+      file = args[0];
+      is = new FileInputStream(file);
     }
+
+    CharStream input = CharStreams.fromStream(is);
+    SimpleExprLexer lexer = new SimpleExprLexer(input);
+
+    for (Token token : lexer.getAllTokens()) {
+      System.out.println(token);
+    }
+  }
 }
